3d840c2772765a7ff26b140e44d0425d59884c3c
dharmish <shah.dharm@husky.neu.edu>
2018-06-20 19:09:25 -0400

    remote mongo

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://webdev-angular-2018.herokuapp.com");
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
   res.header("Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods",
